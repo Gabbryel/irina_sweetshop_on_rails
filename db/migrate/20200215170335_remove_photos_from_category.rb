bf57badf20d272fb5f12f7bcae5feaea3fc9f313
Gabriel Ursache <gabriel@markov.ro>
2020-02-15 20:30:02 +0200

    cloudinary

---
+++ db/migrate/20200215170335_remove_photos_from_category.rb
@@ -0,0 +1,4 @@
+class RemovePhotosFromCategory < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_15_140843) do
+ActiveRecord::Schema.define(version: 2020_02_15_170335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
