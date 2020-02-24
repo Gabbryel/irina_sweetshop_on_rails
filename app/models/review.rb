4be0063eef7c76632a2d50f731b52e12755c3871
Gabriel Ursache <gabriel@markov.ro>
2020-02-24 11:40:35 +0200

    finished reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,9 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :recipe
+  validates :user_id, uniqueness: { scope: :recipe }
   validates :rating, presence: true
   validates :content, presence: true
-  validates :content, length: { minimum: 20 }
-  validates :content, length: { maximum: 100 }
+  validates :content, length: { minimum: 10 }
+  validates :content, length: { maximum: 50 }
 end
