1e0230512aaa6bee5c8d520799f3a45add360815
Gabriel Ursache <gabriel@markov.ro>
2020-02-28 23:16:56 +0200

    heroku upload

--- db/migrate/20200222071855_create_reviews.rb
+++ db/migrate/20200222071855_create_reviews.rb
@@ -4,7 +4,7 @@ class CreateReviews < ActiveRecord::Migration[6.0]
       t.integer :rating
       t.text :content
       t.references :user, null: false, foreign_key: true
-      t.references :recipe, null: false, foreign_key: true
+      
 
       t.timestamps
     end
