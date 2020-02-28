cf3c742d00f64de773bf03f41797b3339368c733
Gabriel Ursache <gabriel@markov.ro>
2020-02-28 23:31:12 +0200

    heroku upload

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
   has_one_attached :photo
-  has_many :recipes, dependent: :destroy
-  has_many :cakemodels, dependent: :destroy
+  has_many :recipes
+  has_many :cakemodels
   validates :name, presence: true
   validates :photo, presence: true
 end
