b98448878daf48e2ed764a940429386d1a945921
Gabriel Ursache <gabriel@markov.ro>
2020-03-02 22:37:13 +0200

    done cakemodel index

--- app/models/cakemodel.rb
+++ app/models/cakemodel.rb
@@ -1,9 +1,24 @@
 class Cakemodel < ApplicationRecord
   belongs_to :category
   has_one_attached :photo
-  
-  include Reviewable
-  
   validates :content, presence: true
   validates :photo, presence: true
+  
+  include Reviewable
+
+  def overall_rating
+    ratings.count == 0 ? "" : ratings.sum / ratings.count.to_f
+  end
+
+  def no_of_ratings
+    ratings.count == 1 ? "(#{ratings.count} recenzie)" : "(#{ratings.count} recenzii)"
+  end
+
+  private
+
+  def ratings
+    ratings = []
+    self.reviews.each { |rev| ratings << rev.rating}
+    ratings
+  end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,17 +7,20 @@ class Recipe < ApplicationRecord
 
   include Reviewable
 
+  
+  def overall_rating
+    ratings.count == 0 ? "" : ratings.sum / ratings.count.to_f
+  end
+  
+  def no_of_ratings
+    ratings.count == 1 ? "(#{ratings.count} recenzie)" : "(#{ratings.count} recenzii)"
+  end
+
+  private 
+  
   def ratings
     ratings = []
     self.reviews.each { |rev| ratings << rev.rating }
     ratings
-  end
-
-  def overall_rating
-    ratings.count == 0 ? "" : ratings.sum / ratings.count.to_f
-  end
-
-  def no_of_ratings
-    ratings.count == 1 ? "(#{ratings.count} recenzie)" : "(#{ratings.count} recenzii)"
   end
 end
