4c2203912de9603f03bd2f05bb80cce6acddd256
Gabriel Ursache <gabriel@markov.ro>
2020-03-29 11:39:55 +0300

    deleted after_create

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :reviews, dependent: :destroy
 
-  after_create :send_welcome_email
+  # after_create :send_welcome_email
 
   private
 
