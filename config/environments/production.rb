68d4527a8dc617c0abd96a48f510e4980e229e1b
Gabriel Ursache <gabriel@markov.ro>
2020-04-07 17:37:04 +0300

    implementing mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,10 +1,6 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: "www.irinasweet.ro" }
-  # Settings specified here will take precedence over those in config/application.rb.
-
-  config.action_mailer.delivery_method = :postmark
-  config.action_mailer.postmark_settings   = { api_token: ENV['POSTMARK_API_TOKEN'] }
-
+  config.action_mailer.delivery_method = :smtp
   # Code is not reloaded between requests.
   config.cache_classes = true
 
--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,10 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  address: "mail.irinasweet.ro",
-  port: 26,
-  domain: 'irinasweet.ro',
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: 'gmail.com',
   user_name: ENV['MAIL_ADDRESS'],
   password: ENV['APP_PASS'],
-  authentication: :plain,
-  enable_starttls_auto: true,
-  openssl_verify_mode: 'none'
-}
+  authentication: :login,
+  enable_starttls_auto: true
+  }
