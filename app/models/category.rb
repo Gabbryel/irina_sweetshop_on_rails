58856c14679d433e446755b4905021a9bcb7e3be
Gabriel Ursache <gabriel@markov.ro>
2020-11-08 09:07:49 +0200

    new card component

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,5 +3,5 @@ class Category < ApplicationRecord
   has_many :recipes, dependent: :destroy
   has_many :cakemodels, dependent: :destroy
   validates :name, presence: true
-  validates :photo, presence: true
+  # validates :photo, presence: true
 end
