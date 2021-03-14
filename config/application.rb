54895434bc5c4336f58621e7fb506543fc102d60
Gabriel Ursache <gabriel@markov.ro>
2021-03-14 13:06:53 +0200

    404 page in place

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module IrinaSweetshopOnRails
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.action_mailer.delivery_method = :letter_opener
-
+    config.exceptions_app = self.routes #configuring 404 route
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
