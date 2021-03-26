013341b28223b17f966e9803a77e03e683417d0d
Gabriel Ursache <gabriel@markov.ro>
2021-03-26 09:58:05 +0200

    minor fix in toggling card title and subtitles

--- app/javascript/packs/homepageJS/easterModal.js
+++ app/javascript/packs/homepageJS/easterModal.js
@@ -1,13 +1,15 @@
 
 export default function renderModal() {
-  const modal = document.getElementById('modal-2021-overlay');
-  const closeModal = document.getElementById('close-easter-modal');
-  closeModal.addEventListener('click', () => {
-    modal.style.display = 'none';
-  })
-  setTimeout(() => {
-    modal.style.display = 'none';
-  },10000)
+  if (document.getElementById('modal-2021-overlay')) {
+    const modal = document.getElementById('modal-2021-overlay');
+    const closeModal = document.getElementById('close-easter-modal');
+    closeModal.addEventListener('click', () => {
+      modal.style.display = 'none';
+    })
+    setTimeout(() => {
+      modal.style.display = 'none';
+    },20000)
+  }
 }
 
 
