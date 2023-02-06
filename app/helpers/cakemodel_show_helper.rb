122356bf1c9fa972476197c04b1aa02b9f939f15
Gabriel Ursache <gabriel@markov.ro>
2023-02-06 21:21:31 +0200

    footer mailto and phone, optimied photos rendering byfetch_format: :auto, width: 600, crop: :scale && worked on cakemodel_show_helper

--- app/helpers/cakemodel_show_helper.rb
+++ app/helpers/cakemodel_show_helper.rb
@@ -7,11 +7,27 @@ module CakemodelShowHelper
     count == 0 ? 'adaugă componente' : count == 1 ? 'rețetei' : count > 1 ? 'rețetelor' : nil
   end
 
-  def one_comp(cm)
-    cm.model_components[0]
+  def components(cm)
+    cm.model_components
   end
 
-  def price_one_comp(cm)
-    (one_comp(cm).recipe_price * one_comp(cm).weight) + ((cm.design.price_cents/100) * one_comp(cm).weight)
+  def total_weight(cm)
+    components(cm).map { |el| el.weight }.sum
+  end
+
+  def one_comp(cm)
+    components(cm)[0]
+  end
+
+  def recipes_price(cm)
+    components(cm).map { |el| el.recipe_price * el.weight}.sum
+  end
+
+  def design_price(cm)
+    (cm.design.price_cents/100) * total_weight(cm)
+  end
+
+  def total_price(cm)
+    recipes_price(cm) + design_price(cm)
   end
 end
