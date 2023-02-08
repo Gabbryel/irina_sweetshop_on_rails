1cf9f8c0b98ddbd947bab1af7573ffde19b9b42b
Gabriel Ursache <gabriel@markov.ro>
2023-02-08 14:52:24 +0200

    error db fixed && models restored

--- app/models/model_component.rb
+++ app/models/model_component.rb
@@ -1,8 +1,7 @@
 class ModelComponent < ApplicationRecord
   belongs_to :cakemodel
   belongs_to :recipe
-  # after_save :set_recipe_dependant, unless: :verify_recipe
 
-  self.ignored_columns = ["recipe_id"]
+  # self.ignored_columns = ["recipe_id"]
 
 end
