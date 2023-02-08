13331b279135c4f1fa4e1e6d079ebecbbefc91f3
Gabriel Ursache <gabriel@markov.ro>
2023-02-08 14:37:01 +0200

    error db fix

--- db/migrate/20230126203843_add_recipe_to_cakemodels.rb
+++ db/migrate/20230126203843_add_recipe_to_cakemodels.rb
@@ -1,5 +1,5 @@
 class AddRecipeToCakemodels < ActiveRecord::Migration[6.1]
   def change
-    add_reference :cakemodels, :recipe, null: false, foreign_key: true
+    add_reference :cakemodels, :recipe, null: true, foreign_key: true
   end
 end
