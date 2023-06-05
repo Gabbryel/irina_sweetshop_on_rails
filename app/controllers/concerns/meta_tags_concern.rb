aa6168b75c3483a3b4e3810110c58771e52b4140
Constantin-Gabriel Ursache <gabriel@markov.ro>
2023-06-05 13:01:06 +0300

    Update meta_tags_concern.rb

--- app/controllers/concerns/meta_tags_concern.rb
+++ app/controllers/concerns/meta_tags_concern.rb
@@ -6,7 +6,7 @@ module MetaTagsConcern
   end
 
   def set_title
-    @page_title = 'Cofetărie - Laborator Irina ・ Bacău, str. Mioriței, nr. 10'
+    @page_title = 'Cofetărie - Laborator Irina ・ Bacău, bd. Unirii, nr. 2'
     @seo_keywords = 'Bacau, Irina, cofetaria, cofetarie, cofetarii, prajitura, prajituri, tort, onomastica, botez, nunta, majorat, cozonac, cozonaci, julfa, pasca, colac, colaci, boema, africana, romania, mirabel, pavlova, dulcineea, dulcinella'
   end
 end
