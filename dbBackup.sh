69b883b0faa6495a24cdfebe82d6dc3327e88f7a
Gabriel Ursache <gabriel@markov.ro>
2025-01-25 21:16:31 +0200

    added google analytics

--- dbBackup.sh
+++ dbBackup.sh
@@ -1,5 +1,8 @@
 rm latest.dump
-heroku login
+heroku login -i
 heroku pg:backups:capture -a irinasweet
 heroku pg:backups:download -a irinasweet
 pg_restore --verbose --clean --no-acl --no-owner -h localhost -d irina_sweetshop_on_rails_development latest.dump
+
+
+# Token:       HRKU-819e6fdd-7ae5-4c56-ab24-b36d280d6725
