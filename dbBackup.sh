7973a13e53db29fe16ae00918d3371b20edb4cd8
Gabriel Ursache <gabriel@markov.ro>
2025-04-11 10:37:50 +0300

    changed main photo

--- dbBackup.sh
+++ dbBackup.sh
@@ -4,5 +4,3 @@ heroku pg:backups:capture -a irinasweet
 heroku pg:backups:download -a irinasweet
 pg_restore --verbose --clean --no-acl --no-owner -h localhost -d irina_sweetshop_on_rails_development latest.dump
 
-
-# Token:       HRKU-819e6fdd-7ae5-4c56-ab24-b36d280d6725
