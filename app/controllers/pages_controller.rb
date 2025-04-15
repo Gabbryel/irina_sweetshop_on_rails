ec969561d7c7d8bf4b61c1acba91782309341ceb
Gabriel Ursache <gabriel@markov.ro>
2025-04-15 14:02:48 +0300

    max 8 items

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :how_to_order, :about, :valori_nutritionale]
 
   def home
-    @recipes = Recipe.where(favored: true).order(position: :asc).sample(8)
+    @recipes = Recipe.where(favored: true).order(position: :asc).first(8)
     @features = Feature.all.order(:id)
     @categories = Category.all.order(:id)
   end
